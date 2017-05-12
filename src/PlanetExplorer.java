b5649ef426cacde8c51e60796e7c8681c2294f52
somename <someemail>
2017-05-12 20:11:07 +0200

    besouro automatic message

--- src/PlanetExplorer.java
+++ src/PlanetExplorer.java
@@ -1,17 +1,33 @@
+import java.util.ArrayList;
 
 // Before you commit and push write your student ID and finish time here.
 // Finish time:
 // Student ID:
 
 public class PlanetExplorer {
+	private int gridX;
+	private int gridY;
+	private String strana = "N";
+	private ArrayList<String> pre = new ArrayList<String>();
+	private int kordinataX=0;
+	private int kordinataY=0;
+	
+	
 	public PlanetExplorer(int x, int y, String obstacles){
 	/*	x and y represent the size of the grid.
 	 *  Obstacles is a String formatted as follows: "(obs1_x,obs1_y)(obs2_x,obs2_y)...(obsN_x,obsN_y)" with no white spaces. 
 	 *  
 		Example use: For a 100x100 grid with two obstacles at coordinates (5,5) and (7,8)
 		PlanetExplorer explorer = new PlanetExplorer(100,100,"(5,5)(7,8)")  
-		 
 	 */
+		gridX=x;
+		gridY=y;
+		String[] prepreke = obstacles.split("(|\\,|\\)");
+		for(int i=0;i<prepreke.length;i++){
+			System.out.println(prepreke[i]);
+		}
+		
+		
 	}
 	
 	public String executeCommand(String command){
@@ -25,6 +41,7 @@ public class PlanetExplorer {
 		 * Where pos_x and pos_y are the final coordinates, facing is the current direction the explorer is pointing to (N,S,W,E).
 		 * The return string should also contain a list of coordinates of the encountered obstacles. No white spaces.
 		 */
+		
 		
 		return null;
 	}
