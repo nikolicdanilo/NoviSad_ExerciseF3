7c9eaf5e0c1a00f452176acd9693ec1d09e51def
somename <someemail>
2017-05-12 20:14:55 +0200

    besouro automatic message

--- tests/TestPlanetExplorer.java
+++ tests/TestPlanetExplorer.java
@@ -3,9 +3,12 @@ import static org.junit.Assert.*;
 import org.junit.Test;
 
 public class TestPlanetExplorer {
-
+	PlanetExplorer vozilo;
 	@Test
 	public void test_executeCommand() {
-		fail("Not yet implemented");
+		vozilo = new PlanetExplorer(100,100,"(10,10)(20,20");
+		for(int i=0;i<vozilo.getPrepreke().length;i++){
+			System.out.println(vozilo.getPrepreke()[i]);
+		}
 	}
 }
