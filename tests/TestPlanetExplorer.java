50afc3d5d0f7e2db9e0b11f90f66d37f60afd76a
somename <someemail>
2017-05-12 20:36:49 +0200

    besouro automatic message

--- tests/TestPlanetExplorer.java
+++ tests/TestPlanetExplorer.java
@@ -6,10 +6,20 @@ public class TestPlanetExplorer {
 	PlanetExplorer vozilo;
 	@Test
 	public void test_executeCommandF() {
-		vozilo = new PlanetExplorer(100,100);
+		vozilo = new PlanetExplorer(10,10);
 		String ocekivaniIspis="(0, 1, N)";
 		
 		assertEquals(ocekivaniIspis,vozilo.executeCommand("f"));
+		
+	}
+	
+	@Test
+	public void test_executeCommandFBorderCase() {
+		vozilo = new PlanetExplorer(2,2);
+		String ocekivaniIspis="(0, 0, N)";
+		
+		
+		assertEquals(ocekivaniIspis,vozilo.executeCommand("fff"));
 		
 	}
 	@Test
